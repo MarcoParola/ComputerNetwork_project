7d453333fdd9f98a66207454895d85ef47c92699
MarcoParola <marcoparola96@gmail.com>
2018-12-25 18:07:59 +0100

    Creati file del CL e del SV

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my project for the exam of Computer Network
---
+++ tftp_client.c
@@ -0,0 +1,22 @@
+/*#include <stdio.h>
+#include <string.h>   //strlen
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>   //close
+#include <arpa/inet.h>    //close
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+  */
+
+
+int main(int argc , char **argv){
+    /*if(argc != 2){
+        printf("Argomenti non corretti, riprovare con:\n");
+        printf("IP_UDP, PORTA_UDP, IP_SERVER, PORTA_SERVER\n correggi");
+        return 0;
+    }*/
+
+
+}
